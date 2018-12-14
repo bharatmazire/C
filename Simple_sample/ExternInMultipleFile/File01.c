ecb7d1db737a8fa1091349297fd512e20b91faf0
Bharat Mazire <mazire.bharat@gmail.com>
2018-12-14 13:23:14 +0530

    Create File01.c

---
+++ Simple_sample/ExternInMultipleFile/File01.c
@@ -0,0 +1,9 @@
+#include<stdio.h>                                                                                                                                                    
+                                                                                                                                                                     
+extern int global_count;                                                                                                                                             
+                                                                                                                                                                     
+void CountByOne()                                                                                                                                                    
+{                                                                                                                                                                    
+        printf("Global Count increased by 1\n");                                                                                                                     
+        global_count++;                                                                                                                                              
+}   
