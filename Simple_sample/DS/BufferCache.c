7b5d633f4035eb4a84041c5e6d9d36b93b61e418
Bharat Mazire <mazire.bharat@gmail.com>
2019-02-12 12:42:39 +0530

    Update BufferCache.c

--- Simple_sample/DS/BufferCache.c
+++ Simple_sample/DS/BufferCache.c
@@ -7,42 +7,62 @@ using namespace std;
 
 struct BUFFER
 {
-  int iDeviceNumber;
-  int iBlockNumber;
-  int iStatus;
-
-  char cData[DATASIZE];
-  
-  struct BUFFER * nextHash;
-  struct BUFFER * prevHash;
-  struct BUFFER * nextFree;
-  struct BUFFER * prevFree;
+    int iBuffDeviceNumber;
+    int iBuffBlockNumber;
+    int iBuffStatus;
+    
+    char cData[DATASIZE];
+ 
+    struct BUFFER * nextHash;
+    struct BUFFER * prevHash;
+    struct BUFFER * nextFree;
+    struct BUFFER * prevFree;
 };
 
 
 int main()
 {
-  int iBufferPoolSize = 9;  // hardcoded 9 for initial purpose
-  int iNumberOfHashQuese = 3; // hardcoded 3 for initial purpose
+    int iBufferPoolSize = 9;  // hardcoded 9 for initial purpose
+    int iNumberOfHashQuese = 3; // hardcoded 3 for initial purpose
 
-  int iInitialNoOfBuffInEachQueue = iBufferPoolSize / iNumberOfHashQuese;
+    int iInitialNoOfBuffInEachQueue = iBufferPoolSize / iNumberOfHashQuese;
 
-  //int iTotalNumberOfHeads = iNumberOfHashQuese + 1; // + 1 for head of freelist
+    //int iTotalNumberOfHeads = iNumberOfHashQuese + 1; // + 1 for head of freelist
 
-  struct BUFFER * ArrayOfHashList[iNumberOfHashQuese] = {NULL};
+    struct BUFFER * ArrayOfHashList[iNumberOfHashQuese] = {NULL};
   
-  struct BUFFER * FreeListHead = NULL;
+    struct BUFFER * FreeListHead = NULL;
 
-  for(int i = 0 ; i < iNumberOfHashQuese ; i ++)
-  {
-  // for ArrayOfHashList[i] : 0,1,2
-    for(int j = 0 ; j < iInitialNoOfBuffInEachQueue ; j++)
+    for(int i = 0 ; i < iNumberOfHashQuese ; i ++)
     {
-      struct BUFFER * buff = (struct BUFFER *)malloc(sizeof)(struct BUFFER *);
+        // for ArrayOfHashList[i] : 0,1,2
+        for(int j = 0 ; j < iInitialNoOfBuffInEachQueue ; j++)
+        {
+            struct BUFFER * buff = (struct BUFFER *)malloc(sizeof(struct BUFFER));
+            buff->iBuffDeviceNumber = 0;
+            buff->iBuffBlockNumber = -1;
+            buff->iBuffStatus = 0;
+            
+            if(j == 0)
+            {
+                buff->nextHash = NULL;
+                buff->prevHash = ArrayOfHashList[i];
+                buff->nextFree = NULL;
+                buff->prevFree = FreeListHead;
+            }
+            else
+            {
+                struct BUFFER * traverse = (struct BUFFER *)malloc(sizeof(struct BUFFER));
+                while(ArrayOfHashList[0]->nextHash != NULL)
+                {
+                    
+                }
+                
+            }
+        }
     }
-  }
 
 
-  printf("Hello World !!");
-  return(0);
+    printf("Hello World !!");
+    return(0);
 }
