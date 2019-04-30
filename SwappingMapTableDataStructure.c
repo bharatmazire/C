41f0062604cb3a9315da7c1ee99b3939bb90a7aa
Bharat Mazire <mazire.bharat@gmail.com>
2019-04-30 18:46:51 +0530

    Update SwappingMapTableDataStructure.c

--- SwappingMapTableDataStructure.c
+++ SwappingMapTableDataStructure.c
@@ -1,93 +1,144 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 #define HSTART 1
 #define HEND 10000
 
 struct MAP_TABLE
 {
-    int iStart;
-    int iUpto;
-    struct MAP_TABLE * next;
+  int iStart;
+  int iUpto;
+  struct MAP_TABLE * next;
 };
 
 int main()
 {
-    struct MAP_TABLE * head = NULL;
-    struct MAP_TABLE *trav;
-    struct MAP_TABLE * new_entry = (struct MAP_TABLE *)malloc(sizeof(struct MAP_TABLE));
+  struct MAP_TABLE * head = NULL;
+  struct MAP_TABLE *trav;
+  struct MAP_TABLE *prev_trav;
+
+  struct MAP_TABLE * new_entry = (struct MAP_TABLE *)malloc(sizeof(struct MAP_TABLE));
     
-    new_entry->iStart = 1;
-    new_entry->iUpto = 10000;
-    new_entry->next = NULL;
+  new_entry->iStart = 1;
+  new_entry->iUpto = 10000;
+  new_entry->next = NULL;
 
-    head = new_entry;
+  head = new_entry;
 
-    int ch = 0;
-    do
-    {
-      printf("\n1.malloc 2. free 3. display 4.exit\n\n");
-      scanf("%d",&ch);
+  int ch = 0;
+  do
+  {
+    printf("\n1.malloc 2. free 3. display 4.exit\n\n");
+    scanf("%d",&ch);
       
-      if(ch == 1)
+    // for 1 choice add
+    if(ch == 1)
+    {
+      printf("\nadd\n");
+      int start;
+      int upto;
+      int flag = 0;
+      trav = head;
+
+      printf("\nEnter amount of data : ");
+      scanf("%d",&upto);
+
+      while(trav->iUpto < upto && trav->next != NULL)
       {
-        printf("\nadd\n");
-        int start;
-        int upto;
-        int flag = 0;
-        trav = head;
-
-        printf("\nEnter amount of data : ");
-        scanf("%d",&upto);
-
-        printf("\nupto is %d",upto);
-        printf("\ntrav->upto is %d",trav->iUpto);
-
-        while(trav->iUpto < upto && trav->next != NULL)
-        {
-          printf("\nupto is %d",upto);
-          printf("\ntrav->upto is %d",trav->iUpto);
-          trav = trav->next;
-        }
-        if(trav->iUpto >= upto)
-        {
-          flag = 1;
-        }
-        if(flag == 1)
-        {
-          //do something
-          trav->iUpto = trav->iUpto - upto;
-          trav->iStart = trav->iStart + upto;
-        }
-        else
-        {
-          printf("\nNo place for swap out\n");
-        }
+        trav = trav->next;
       }
-      else if(ch == 2)
+      if(trav->iUpto >= upto)
       {
-        printf("\nfree\n");
+        flag = 1;
       }
-        
-      else if(ch == 3)
+      if(flag == 1)
       {
-        printf("\ndisplay\n");
-        trav = head;
-        printf("\nstart : %d :: upto val : %d\n",head->iStart , head->iUpto);
-        while(trav->next != NULL)
-        {
-          printf("\nstart : %d :: upto val : %d\n",trav->iStart , trav->iUpto);
-          trav = trav->next;
-        }
-        trav = NULL;
-        free(trav);
+        trav->iUpto = trav->iUpto - upto;
+        trav->iStart = trav->iStart + upto;
       }
       else
       {
-        printf("out\n\n");
+        printf("\nNo place for swap out\n");
       }
-    }while(ch != 4);
+    }
+  
+    // for choice 2, free
+    else if(ch == 2)
+    {
+      int freeStart;
+      int freeUpto;
+      int f_for_maintain_prev = 0;
+      trav = head;
+      prev_trav = NULL;
 
-    return(0);
+      struct MAP_TABLE * free_map = (struct MAP_TABLE *)malloc(sizeof(MAP_TABLE));
+
+      printf("\nfree\n");
+
+      printf("\nEnter start : \n");
+      scanf("%d",&freeStart);
+
+      printf("\nEnter upto : \n");
+      scanf("%d",&freeUpto);
+
+      free_map->iStart = freeStart;
+      free_map->iUpto = freeUpto;
+      free_map->next = NULL;
+
+      while(trav && trav->iStart <= free_map->iStart){
+        prev_trav = trav;
+        trav = trav->next;
+      }
+      if(!trav) // trav == NULL;
+      {
+        prev_trav->next = free_map;
+      }
+      else
+      {
+        if(prev_trav) 
+        {
+          free_map->next = prev_trav->next;
+          prev_trav-> next = free_map;
+        }
+        else 
+        {
+          free_map->next = head;
+          head = free_map;
+        }
+      }
+
+      trav = head;
+      while(trav->next != NULL)
+      {
+        if(trav->iStart + trav->iUpto == trav->next->iStart)
+        {
+          trav->iUpto = trav->next->iUpto + trav->iUpto;
+          trav->next = trav->next->next;
+        }
+        trav = trav->next;
+      }
+    }
+    // for choice 3, display
+    else if(ch == 3)
+    {
+      printf("\ndisplay\n");
+      trav = head;
+      //printf("\nstart : %d :: upto val : %d\n",head->iStart , head->iUpto);
+      while(trav != NULL)
+      {
+        printf("\nstart : %d :: upto val : %d\n",trav->iStart , trav->iUpto);
+        trav = trav->next;
+      }
+    }
+
+    // for other choice
+    else
+    {
+      trav = NULL;
+      free(trav);
+      printf("out\n\n");
+    }
+  }while(ch != 4);
+
+  return(0);
 }
