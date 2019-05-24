6d210069979ba3b747faa63470a8d8ea2ece22eb
Bharat Mazire <mazire.bharat@gmail.com>
2019-05-25 01:24:36 +0530

    Final Tower of Hanoi {max limit tested 5 }
    got failed on 10

--- TowerOfHanoi.c
+++ TowerOfHanoi.c
@@ -1,196 +1,244 @@
-/*
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 
-
-#define NUM 6
-
-int main()
-{
-  // declaration of variables
-  int iNumberOfPiece;
-  
-  int *Source = NULL;
-  int *Auxilary = NULL;
-  int *Destination = NULL;
-
-  int iTotalNumberOfIteration;
-
-  int topStack[3] = {0,0,0};
-  // topStack[0] = for source top
-  // topStack[1] = for Auxilary top
-  // topStack[2] = for destination top
-  // initially set all top at 0
-  // we can set top seperate but just for learning purpose i am making it in array
-
-  // code
-
-  //printf("\nEnter the number of piece you want : \n");
-  //scanf("%d",&iNumberOfPiece);
-  iNumberOfPiece = NUM;
-
-  Source = (int *)malloc(sizeof(int) * iNumberOfPiece);
-  Auxilary = (int *)malloc(sizeof(int) * iNumberOfPiece);
-  Destination = (int *)malloc(sizeof(int) * iNumberOfPiece);
-
-  iTotalNumberOfIteration = pow(2,iNumberOfPiece);
-
-  for(int i = 0 ; i < iNumberOfPiece; i++)
-  {
-    Source[i] = iNumberOfPiece-i;
-    Auxilary[i] = 99;
-    Destination[i] = 99;
-    topStack[0] = i;
-  }
-
-  for(int i = 0 ; i < iNumberOfPiece ;i++)
-  {
-    printf("S : %d | ",Source[i]);
-    printf("A : %d |",Auxilary[i]);
-    printf("D : %d |",Destination[i]);
-    printf("\n");
-  }
-  
-  printf("\nTOP of Stacks \n S | A | D |\n");
-
-  for(int i = 0 ; i < 3 ; i++)
-  {
-    printf(" %d |",topStack[i]);
-  }
-
-
- for(int i = 1 ; i <= iTotalNumberOfIteration ; i++)
- {
-   if((i % 3) == 1)
-   {
-     // legal move between source and destination
-    if(Source[topStack[0]] < Destination[topStack[2]])
-    {
-      Destination[topStack[2]] = Source[topStack[0]];
-      topStack[2] ++;
-      topStack[0] --;
-    }
-    else
-    {
-      Source[topStack[0]] = Destination[topStack[2]];
-      topStack[0] ++;
-      topStack[2] --;
-    }
-   }
-   else if((i % 3) == 2)
-   {
-     // legal move between source and Auxilary
-     if(Source[topStack[0]] < Auxilary[topStack[1]])
-    {
-      Auxilary[topStack[1]] = Source[topStack[0]];
-      topStack[1] ++;
-      topStack[0] --;
-    }
-    else
-    {
-      Source[topStack[0]] = Auxilary[topStack[1]];
-      topStack[0] ++;
-      topStack[1] --;
-    }
-   }
-   else if((i % 3) == 0)
-   {
-     // legal move between Auxilary and destination
-     if(Destination[topStack[2]] < Auxilary[topStack[1]])
-    {
-      Auxilary[topStack[1]] = Destination[topStack[2]];
-      topStack[1] ++;
-      topStack[2] --;
-    }
-    else
-    {
-      Destination[topStack[2]] = Auxilary[topStack[1]];
-      topStack[2] ++;
-      topStack[1] --;
-    }
-   }
-   else
-   {
-     printf("\nYou will(/should) not come here !!\n");
-   }
- 
- 
- }
-
-  return(0);
-}
-*/
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#define NUM 3
 
 int main()
 {
-  // declaration of variables
-  int iNumberOfPiece;
-  
-  int *Source = NULL;
-  int *Auxilary = NULL;
-  int *Destination = NULL;
-
-  int iTotalNumberOfIteration;
-
-  int topStack[3] = {0,0,0};
-  // topStack[0] = for source top
-  // topStack[1] = for Auxilary top
-  // topStack[2] = for destination top
-  // initially set all top at 0
-  // we can set top seperate but just for learning purpose i am making it in array
-
-  // code
-
-  printf("\nEnter the number of piece you want : \n");
-  scanf("%d",&iNumberOfPiece);
-
-  Source = (int *)malloc(sizeof(int) * iNumberOfPiece);
-  Auxilary = (int *)malloc(sizeof(int) * iNumberOfPiece);
-  Destination = (int *)malloc(sizeof(int) * iNumberOfPiece);
-
-  iTotalNumberOfIteration = pow(2,iNumberOfPiece);
-
-  for(int i = 0 ; i < iNumberOfPiece; i++)
-  {
-    Source[i] = iNumberOfPiece-1-i;
-    topStack[0] = i;
-  }
-
-  for(int i = 0 ; i < iNumberOfPiece ;i++)
-  {
-    printf("%d ",Source[i]);
-  }
-
- for(int i = 1 ; i <= iTotalNumberOfIteration ; i++)
- {
-   if((i % 3) == 1)
-   {
-     // legal move between source and destination
-    
+	// declaration of variables
+	int iNumOfPiece = NUM;		// handle this on keypress
 
 
+	int *Src = NULL;
+	int *Aux = NULL;
+	int *Des = NULL;
 
-   }
-   else if((i % 3) == 2)
-   {
-     // legal move between source and Auxilary
-   }
-   else if((i % 3) == 0)
-   {
-     // legal move between Auxilary and destination
-   }
-   else
-   {
-     printf("\nYou will(/should) not come here !!\n");
-   }
- 
- 
- }
+	int iTotalNumOfItr;
 
-  return(0);
+	int SrcTop = -1;
+	int AuxTop = -1;
+	int DesTop = -1;
+
+	// code
+	iTotalNumOfItr = pow(2,iNumOfPiece) - 1;
+
+	Src = (int *)malloc(sizeof(int) * iNumOfPiece);
+	Aux = (int *)malloc(sizeof(int) * iNumOfPiece);
+	Des = (int *)malloc(sizeof(int) * iNumOfPiece);
+
+	for(int i = 0 ; i < iNumOfPiece ; i++)
+	{
+		Src[i] = iNumOfPiece - i;
+		SrcTop++;
+		Aux[i] = 99;
+		Des[i] = 99;
+	}
+
+	printf("\n I N I T I A L  S T A G E \n");
+	
+	printf("\nPrinting Stacks\n");
+	for(int i = iNumOfPiece-1 ; i >= 0 ; i--)
+	{
+		printf("S : %d | ",Src[i]);
+		printf("A : %d | ",Aux[i]);
+		printf("D : %d | ",Des[i]);
+		printf("\n");
+	}
+
+	printf("\nPrinting Stack pointer\n");
+	printf("S : %d | A : %d | D : %d\n",SrcTop, AuxTop , DesTop);
+
+	for(int i = 1 ; i <= iTotalNumOfItr ; i++)
+	{
+		// 0 : a - d
+		// 1 : s - d
+		// 2 : s - a
+		
+		int iMod = i % 3;
+		printf("\n---------------------------------------\n");
+		printf("\n i = %d and iMod = %d\n",i , iMod);
+		switch(iMod)
+		{
+			case 0:
+				printf("\nLegal move between Aux and Des\n");
+				if(AuxTop == -1)
+				{
+					AuxTop++;
+					Aux[AuxTop] = Des[DesTop];
+					Des[DesTop] = 99;
+					DesTop--;
+				}
+				else if(DesTop == -1)
+				{
+					DesTop++;
+					Des[DesTop] = Aux[AuxTop];
+					Aux[AuxTop] = 99;
+					AuxTop--;
+				}
+				else if(Aux[AuxTop] < Des[DesTop])
+				{
+					printf("\nAux %d to Des %d \n",Aux[AuxTop],Des[DesTop]);
+
+					DesTop++;
+					
+					int temp = Des[DesTop];
+					Des[DesTop] = Aux[AuxTop];
+					Aux[AuxTop] = temp;
+					//DesTop++;
+					AuxTop--;
+				}
+				else
+				{
+					printf("\nDes %d to Aux %d \n",Des[DesTop],Aux[AuxTop]);
+					
+					AuxTop++;
+
+					int temp = Aux[AuxTop];
+					Aux[AuxTop] = Des[DesTop];
+					Des[DesTop] = temp;
+					//AuxTop++;
+					DesTop--;
+				}
+				printf("\nPrinting Stacks\n");
+				for(int i = iNumOfPiece-1 ; i >= 0 ; i--)
+				{
+					printf("S : %d | ",Src[i]);
+					printf("A : %d | ",Aux[i]);
+					printf("D : %d | ",Des[i]);
+					printf("\n");
+				}
+
+				printf("\nPrinting Stack pointer\n");
+				printf("S : %d | A : %d | D : %d\n",SrcTop, AuxTop , DesTop);
+				break;
+
+			case 1:
+				printf("\nLegal move between Src and Des\n");
+				if(SrcTop == -1)
+				{
+					SrcTop++;
+					Src[SrcTop] = Des[DesTop];
+					Des[DesTop] = 99;
+					DesTop--;
+				}
+				else if(DesTop == -1)
+				{
+					DesTop++;
+					Des[DesTop] = Src[SrcTop];
+					Src[SrcTop] = 99;
+					SrcTop--;
+				}
+				else if(Src[SrcTop] < Des[DesTop])
+				{
+					printf("\nSrc %d to Des %d \n",Src[SrcTop],Des[DesTop]);
+					
+					DesTop++;
+
+					int temp = Des[DesTop];
+					Des[DesTop] = Src[SrcTop];
+					Src[SrcTop] = temp;
+					//DesTop++;
+					SrcTop--;
+				}
+				else
+				{
+					printf("\nDes %d to Src %d \n",Des[DesTop],Src[SrcTop]);
+					
+					SrcTop++;
+
+					int temp = Src[SrcTop];
+					Src[SrcTop] = Des[DesTop];
+					Des[DesTop] = temp;
+					//SrcTop++;
+					DesTop--;
+				}
+				printf("\nPrinting Stacks\n");
+				for(int i = iNumOfPiece-1 ; i >= 0 ; i--)
+				{
+					printf("S : %d | ",Src[i]);
+					printf("A : %d | ",Aux[i]);
+					printf("D : %d | ",Des[i]);
+					printf("\n");
+				}
+
+				printf("\nPrinting Stack pointer\n");
+				printf("S : %d | A : %d | D : %d\n",SrcTop, AuxTop , DesTop);
+
+
+				break;
+
+			case 2:
+				printf("\nLegal move between Src and Aux\n");
+				if(SrcTop == -1)
+				{
+					SrcTop++;
+					Src[SrcTop] = Aux[AuxTop];
+					Aux[AuxTop] = 99;
+					AuxTop--;
+				}
+				else if(AuxTop == -1)
+				{
+					AuxTop++;
+					Aux[AuxTop] = Src[SrcTop];
+					Src[SrcTop] = 99;
+					SrcTop--;
+				}
+				else if(Src[SrcTop] < Aux[AuxTop])
+				{
+					printf("\nAux %d to Src %d \n",Aux[AuxTop],Src[SrcTop]);
+					
+					AuxTop++;
+
+					int temp = Aux[AuxTop];
+					Aux[AuxTop] = Src[SrcTop];
+					Src[SrcTop] = temp;
+					//AuxTop++;
+					SrcTop--;
+				}
+				else
+				{
+					printf("\nSrc %d to Aux %d \n",Src[SrcTop],Aux[AuxTop]);
+					
+					SrcTop++;
+
+					int temp = Src[SrcTop];
+					Src[SrcTop] = Aux[AuxTop];
+					Aux[AuxTop] = temp;
+					//SrcTop++;
+					AuxTop--;
+				}
+				printf("\nPrinting Stacks\n");
+				for(int i = iNumOfPiece-1 ; i >= 0 ; i--)
+				{
+					printf("S : %d | ",Src[i]);
+					printf("A : %d | ",Aux[i]);
+					printf("D : %d | ",Des[i]);
+					printf("\n");
+				}
+
+				printf("\nPrinting Stack pointer\n");
+				printf("S : %d | A : %d | D : %d\n",SrcTop, AuxTop , DesTop);
+
+				break;
+
+			default:
+				printf("\nYou Should not come here (You will not come over here ;p)\n");
+				
+				break;
+		}
+	}
+
+	printf("\nPrinting Stacks after operations\n");
+	for(int i = iNumOfPiece-1 ; i >= 0 ; i--)
+	{
+		printf("S : %d | ",Src[i]);
+		printf("A : %d | ",Aux[i]);
+		printf("D : %d | ",Des[i]);
+		printf("\n");
+	}
+
+	printf("\nPrinting Stack pointer\n");
+	printf("S : %d | A : %d | D : %d\n",SrcTop, AuxTop , DesTop);
+	return(0);
 }
